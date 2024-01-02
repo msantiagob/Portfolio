83daa63c1a4bd103fe6ad4d9664033a7ad08f901
Santiago Bedoya <santiago@Elrepapu-Mac.local>
2024-01-02 17:06:22 -0500

    added skills

--- main.js
+++ main.js
@@ -21,3 +21,55 @@ items.forEach((item, index) => {
   item.addEventListener('click', (e) => { handleIndicator(e.target)});
   item.classList.contains('is-active') && handleIndicator(item);
 })
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var images = document.querySelectorAll(".modalImg");
+images.forEach(function (img) {
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+modal.onclick = function () {
+  if (event.target !== document.getElementById("img01")) {
+    modal.style.display = "none";
+  }
+};
