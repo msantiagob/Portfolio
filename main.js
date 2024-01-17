2b49082519dea1e61d2a6837adaad4371ca2c4d7
Santiago Bedoya <miguelsant2001@gmail.com>
2024-01-17 02:26:46 -0500

    working on scroll

--- main.js
+++ main.js
@@ -73,3 +73,57 @@ modal.onclick = function () {
     modal.style.display = "none";
   }
 };
+window.onscroll = function () {
+  let y = window.scrollY;
+  if (100 < y) {
+
+    const containerNav = document.getElementById('containerNav');
+    const logo = document.getElementById('logo');
+    const txtLogo = document.getElementById('txtLogo');
+    containerNav.style.height = '10vh';
+    logo.style.width = '60px';
+    txtLogo.style.fontSize = '18px';
+  }
+  if (y < 100) {
+    const containerNav = document.getElementById('containerNav');
+    containerNav.style.height = '20vh';
+    logo.style.width = '80px';
+    txtLogo.style.fontSize = '24px';
+  }
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
+
+
+  var tuElemento = document.getElementById('aboutMe');
+console.log(isElementInViewport(tuElemento));
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  let links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let targetId = this.getAttribute('href').substring(1);
+      let targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        let offset = 65;
+        window.scrollTo({
+          top: targetElement.offsetTop - offset,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
